7150b409490a84c14e8e455172f0c22fca5c3bc5
jayashan <jayashan2@live.com>
2022-08-07 14:02:13 +1000

    test

--- POS/static/js/myjs.js
+++ POS/static/js/myjs.js
@@ -1,19 +1,3 @@
-// jQuery
-$(document).ready( function () {
-    $( function() {
-        $( "#datepicker" ).datepicker({
-          dateFormat: "yy-mm-dd"
-
-        });
-    });
-
-
-
-    unit_price=0.00;
-    unit_price=document.getElementById("unit_price");
-
-});
-
 
 
 
